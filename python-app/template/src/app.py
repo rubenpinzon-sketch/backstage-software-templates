97a11650d6d0ef7d52223b5815dd3914e8199fa8
Dario Pinzon <rp@rubenchos.net>
2026-01-14 10:45:18 -0500

    Adds python app software template

---
+++ python-app/template/src/app.py
@@ -0,0 +1,35 @@
+
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+
+@app.route('/api/v1/info')
+
+def info():
+    return jsonify({
+        'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
+        'hostname': socket.gethostname(),
+        'message': 'You are doing great, <3 human!',
+        'deployed_on': 'kubernetes!',
+        'env': '${{values.app_env}}',
+        'app_name': '${{values.app_name}}'
+    })
+
+@app.route('/api/v1/healthz')
+
+def healthz():
+    return jsonify({
+        'status': 'up'
+    }), 200
+    
+if __name__ == '__main__':
+
+
+    app.run(host="0.0.0.0")
+    
+    
+# '/api/v1/details'
+# '/api/v1/healthz'
